d649efc302045b87733aac782a548d48ba1890d3
Muhammad Alwi Ramadhan <alwi.romadhon@gmail.com>
2020-04-15 23:27:38 +0700

    Create Latihan1b.php

---
+++ praktikum/P1_SENIN13_193040029/Latihan1b.php
@@ -0,0 +1,26 @@
+<?php 
+
+$angka = 5;
+
+echo "Aku adalah angka " . $angka;
+echo "</br>";
+
+echo "Jika aku ditambah 20, jumlahku sekarang ";
+echo $angka = $angka + 20;
+echo "</br>";
+
+
+echo "Jika aku dikali 4, jumlahku sekarang ";
+echo $angka = $angka * 4;
+echo "</br>";
+
+echo "Jika aku dikurangi 10, jumlahku sekarang ";
+echo $angka = $angka - 10;
+echo "</br>";
+
+echo "Jika aku dibagi 5, jumlahku sekarang ";
+echo $angka % 5;
+
+
+
+?>
